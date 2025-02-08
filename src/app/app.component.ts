854e6677beca107f7c34f92e77094d237a4a1124
Aevin Earl Molina <amolina@blastasia.com>
2025-02-08 17:09:25 +0800

    add bootstrap files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BookInventoryManagementComponent } from './features/book-inventory-management/book-inventory-management.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [BookInventoryManagementComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
